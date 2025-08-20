ff537c3d24b2c245aa0c6d1843cd3929de5c1d50
MARX VINICIUS GAMA VIEIRA BISPO <marx@alunos.utfpr.edu.br>
2025-08-20 02:53:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/cadastro/form.tsx
+++ src/app/cadastro/form.tsx
@@ -251,7 +251,7 @@ export function RegistrationForm() {
         <Card>
           <CardHeader>
             <CardTitle>Contato</CardTitle>
-          </Header>
+          </CardHeader>
           <CardContent className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             <FormField
               control={form.control}
