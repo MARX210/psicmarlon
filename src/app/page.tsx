550f8c222fb18490a1045ade020d3da662d3a925
MARX210 <marx@alunos.utfpr.edu.br>
2025-09-04 11:18:03 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Calendar, UserPlus, Stethoscope, UserCheck } from "lucide-react";
